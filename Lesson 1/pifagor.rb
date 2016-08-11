5f34a91e1ba0733dc2ac634a8115b274368d5dd9
Botanik1592 <anton.buryka@gmail.com>
2016-08-11 19:33:06 +0300

    Ok. It's work.

--- Lesson 1/pifagor.rb
+++ Lesson 1/pifagor.rb
@@ -1,23 +1,19 @@
 # Прямоугольный треугольник
-
-def right_triangle(a, b, c)
-  if a > b && a > c
-    b**2 + c**2 == a**2
-  elsif b > a && b > c
-    hyp = b
-  elsif c > a && c > b
-    hyp = c
-  end
-  return hyp
+def r_triangle?(a, b, c)
+  b**2 + c**2 == a**2 || a**2 + c**2 == b**2 || a**2 + b**2 == c**2
 end
 
 print "Введите длину 1-й стороны треугольника: "
 a = gets.to_i
-
 print "Введите длину 2-й стороны треугольника: "
 b = gets.to_i
-
 print "Введите длину 3-й стороны треугольника: "
 c = gets.to_i
 
-puts hypotenuse_search(a, b, c)
+if a == b || a == c || b == c
+  puts "Это равнобедренный треугольник"
+elsif r_triangle?(a, b, c)
+  puts "Это прямоугольный треугольник"
+else
+  puts "Это не прямоугольный и не равносторонний треугольник"
+end
