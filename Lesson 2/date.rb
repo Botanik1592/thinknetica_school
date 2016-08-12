d6e750f2015aa6c588194bbdae3b27a7c2e75f33
Botanik1592 <anton.buryka@gmail.com>
2016-08-12 21:00:56 +0300

    OK

--- Lesson 2/date.rb
+++ Lesson 2/date.rb
@@ -5,7 +5,6 @@ months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 num = 0
 
-
 print "Введите число (цифры): "
 day = gets.to_i
 print "Введите номер месяца (цифры): "
@@ -14,32 +13,16 @@ print "Введите год (четыре цифры): "
 year = gets.to_i
 
 if (year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0 || year == 2000
-
-  if month < 2
-    num = day
-  elsif month == 2
-    num = 31 + day
-  else
-    months[1] = 29
-
-    (month-1).times do |i|
-      num = num + months[i+1]
-    end
-    num = num + day
+  months[1] = 29
+  (month-1).times do |i|
+    num = num + months[i]
   end
-
+  num = num + day
 else
-
-  if month < 2
-    num = day
-  elsif month == 2
-    num = 31 + day
-  else
-        (month-1).times do |i|
-      num = num + months[i+1]
-    end
-    num = num + day
+  (month-1).times do |i|
+    num = num + months[i]
   end
+  num = num + day
 end
 
-puts num
+puts "Номер этого дня с начала года: #{num}"
