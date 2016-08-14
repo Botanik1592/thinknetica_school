1e029aecee555fa0b3c4922f44850caa15f98351
Botanik1592 <anton.buryka@gmail.com>
2016-08-14 14:54:46 +0300

    Избавился от переменных, теперь только массив.

--- Lesson 2/fib.rb
+++ Lesson 2/fib.rb
@@ -1,12 +1,14 @@
 # Заполнить массив числами фибоначи до 100
 
-arr = [0,1,1]
+arr = [0,1]
 
-fib = arr[0]
-fib1 = arr[1]
-fib2 = arr[2]
-
-97.times do
-  arr << fib = fib1 + fib2
-  fib1, fib2 = fib2, fib
+2.upto(100) do |i|
+  arr[i] = arr[-1] + arr[-2]
 end
+
+puts arr
+
+# Можно сделать через each:
+# (2..100).each do |i|
+#   arr[i] = arr[-1] + arr[-2]
+# end
