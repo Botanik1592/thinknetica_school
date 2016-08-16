47e215b327bc65613706579f6e169d3181839682
Botanik1592 <anton.buryka@gmail.com>
2016-08-16 11:48:31 +0300

    Убраны лишние тесты

--- TransportTycoon/app.rb
+++ TransportTycoon/app.rb
@@ -5,6 +5,7 @@ require './station.rb'
 # Объявляем два поезда
 redarrow = Train.new(111, :passenger, 17)
 cargo1 = Train.new(222, :cargo, 54)
+cargo2 = Train.new(333, :cargo, 2)
 
 # Объявляем станции
 s1 = Station.new("Питер")
@@ -21,8 +22,8 @@ route2 = Route.new(s2, s1)
 route1.add_station(s3)
 route1.add_station(s4)
 
-route2.add_station(s3)
 route2.add_station(s4)
+route2.add_station(s3)
 
 # Задаем маршруты поездам
 redarrow.recieve_route(route1)
@@ -31,31 +32,8 @@ cargo1.recieve_route(route2)
 #Загоняем поезда на станции
 s1.recept_train(redarrow)
 s2.recept_train(cargo1)
-
-# Отправляем Красную Стрелу в Бологое.
-redarrow.go_to(s4)
-
-# Отправляем товарняк в Любань
-cargo1.go_to(s3)
-
-# Выдаем список поездов на станции
-s3.show_passenger_trains
-s3.show_freight_trains
-s4.show_passenger_trains
-s4.show_freight_trains
-
-# Список всех поездов на станции
-s1.show_trains
-s2.show_trains
-
-# Вывод текщей станции
-redarrow.local_station
-cargo1.local_station
-
-# Вывод следующей станции
-redarrow.next_station
-redarrow.prev_station
-
-# Вывод предыдущей станции
-cargo1.next_station
-cargo1.prev_station
+puts
+# Отправляем поезда до станций
+redarrow.go_to(s2)
+puts
+cargo1.go_to(s1)
