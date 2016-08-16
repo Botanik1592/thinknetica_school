b21f7d0ca7ff8ec938a379c2d1c1cbf0b3952fb7
Botanik1592 <anton.buryka@gmail.com>
2016-08-16 11:49:38 +0300

    1. Небольшие косметические правки. 2. Использую self.name вместо переменной @name

--- TransportTycoon/station.rb
+++ TransportTycoon/station.rb
@@ -10,30 +10,26 @@ class Station
   def recept_train(train)
     @trains << train
     train.current_station = self
-    puts "Поезд № #{train.number} прибыл на станцию #{@name}."
+    puts "Поезд №#{train.number} прибыл на станцию #{self.name}."
   end
 
   def show_trains
-    puts "Список поездов на станции #{@name}:"
-    @trains.each_with_index do |train, i|
-      puts "№ #{train.number} - #{train.type} - вагонов #{train.wagons_count}"
+    puts "Список поездов на станции #{self.name}:"
+    @trains.each do |train|
+      puts "№ #{train.number} - #{Train::TYPE[train.type]} - вагонов #{train.wagons_count}"
     end
   end
 
-# Да, можно было бы сделать один метод, в который бы передавался тип поезда, и тогда бы
-# мы отображали их список, но мое решение в данной ситауции считаю более правильным,
-# т.к. оно нагляднее. Внизу в комментариях я привожу метод из ТЗ
-
   def show_passenger_trains
-    puts "Пассажирских поездов на станции #{@name}: #{trains_selector(:passenger)}"
+    puts "Пассажирских поездов на станции #{self.name}: #{trains_selector(:passenger)}"
   end
 
   def show_freight_trains
-    puts "Грузовых поездов на станции #{@name}: #{trains_selector(:cargo)}"
+    puts "Грузовых поездов на станции #{self.name}: #{trains_selector(:cargo)}"
   end
 
   def send_train(train)
-    puts "Поезд № #{train.number} ушел со станции #{@name}."
+    puts "Поезд №#{train.number} ушел со станции #{self.name}."
     @trains.delete(train)
   end
 
@@ -48,40 +44,5 @@ class Station
     end
     return count
   end
+
 end
-
-
-#
-# Вариант реализации одним методом, но он мне не нравится, т.к. нужно передавать
-# параметры, а хотелось сделать вызов без параметров.
-# Криво объяснил, но как-то так )))
-#
-#def show_trains(type="all")
-#  count = 0
-#  if type = "all"
-#    puts "Список поездов на станции #{@name}:"
-#    @trains.each_with_index do |train, i|
-#      puts "№ #{train.number} - #{train.type}"
-#    end
-#  elsif type = "пассажирский"
-#    puts "Список пассажирских поездов на станции #{@name}:"
-#    @trains.each do |train|
-#      if train.type == type
-#        puts "№ #{train.number} - #{train.type}"
-#        count += 1
-#      end
-#    end
-#    puts "Итого: #{count}"
-#  elsif type = "грузовой"
-#    puts "Список грузовых поездов на станции #{@name}:"
-#    @trains.each do |train|
-#      if train.type == type
-#        puts "№ #{train.number} - #{train.type}"
-#        count += 1
-#      end
-#    end
-#    puts "Итого: #{count}"
-#  else
-#    puts "Неверный тип поезда!"
-#  end
-#end
