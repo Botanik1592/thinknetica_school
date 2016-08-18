bd80f496c46b717c7923742bcde28bbcc86f2531
Botanik1592 <anton.buryka@gmail.com>
2016-08-18 15:24:26 +0300

    Добавлен initialize для подсчета объектов подклассов.

--- TransportTycoon/wagon.rb
+++ TransportTycoon/wagon.rb
@@ -2,6 +2,10 @@ class Wagon
   include Manufacturer
   include InstanceCounter
 
+  def initialize
+    register_instance
+  end
+
   protected
 
   TYPE = { passenger: "пассажирский", cargo: "грузовой" }
