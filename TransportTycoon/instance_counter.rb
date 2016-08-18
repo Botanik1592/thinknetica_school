6bda118821fecf36a6b3689b955e3ac7ad7a266a
Botanik1592 <anton.buryka@gmail.com>
2016-08-18 10:24:20 +0300

    Модуль, подключающий инстанс и класс модули для подсчета колличества объектов класса. Сам в шоке, но это работает. ))

--- TransportTycoon/instance_counter.rb
+++ TransportTycoon/instance_counter.rb
@@ -1,28 +1,20 @@
-# Создать модуль InstanceCounter, содержащий следующие методы класса и инстанс-методы, которые подключаются автоматически при вызове include в классе:
-#Методы класса:
-#       - instances, который возвращает кол-во экземпляров данного класса
-#Инастанс-методы:
-#       - register_instance, который увеличивает счетчик кол-ва экземпляров класса и который можно вызвать из конструктора.
-#       При этом данный метод не должен быть публичным.
-
 module InstanceCounter
-
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
 
   module ClassMethods
-    @@count = 0
-    def instances
-      @@count
+    @@count ||= 0
+    def instances(num=0)
+      @@count += num
     end
   end
 
   module InstanceMethods
-    @@count = 0
+    private
     def register_instance
-      @@count += 1
+      self.class.instances(1)
     end
   end
 end
