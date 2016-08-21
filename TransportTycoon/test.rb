35ac1b00b98c37c63b33ddfc6f8ba0173daafbcf
Botanik1592 <anton.buryka@gmail.com>
2016-08-21 14:54:55 +0300

    Тесты

--- TransportTycoon/test.rb
+++ TransportTycoon/test.rb
@@ -9,7 +9,56 @@ require "./cargo_wagon.rb"
 require "./station.rb"
 require "./route.rb"
 
-piter = Station.new ("Питер")
-moscow = Station.new ("Москва")
+# Создаем станции
+st1 = Station.new ("Питер")
+st2 = Station.new ("Москва")
 
-route1 = Route.new(piter, moscow, "Питер-Москва")
+# Создаем поезда
+tr = PassengerTrain.new ("111-22")
+tr2 = CargoTrain.new ("111-33")
+
+# Создаем вагоны
+pw1 = PassengerWagon.new
+pw2 = PassengerWagon.new
+pw3 = PassengerWagon.new
+pw4 = PassengerWagon.new
+pw5 = PassengerWagon.new
+
+cw1 = CargoWagon.new
+cw2 = CargoWagon.new
+cw3 = CargoWagon.new
+cw4 = CargoWagon.new
+cw5 = CargoWagon.new
+
+# Добавляем вагоны к поездам
+tr.add_wagon (pw1)
+tr.add_wagon (pw2)
+tr.add_wagon (pw3)
+tr.add_wagon (pw4)
+tr.add_wagon (pw5)
+
+tr2.add_wagon (cw1)
+tr2.add_wagon (cw2)
+tr2.add_wagon (cw3)
+tr2.add_wagon (cw4)
+tr2.add_wagon (cw5)
+
+# Добавляем поезда на станции
+st1.recept_train (tr)
+st2.recept_train (tr2)
+
+# Блок для вывода информации о поездах и их вагонах на станциях
+train_block = lambda do |train|
+  puts "Станция #{train.current_station.name}:"
+  puts "#{train.number} - #{Train::TYPE[train.type]} - #{train.wagons.size}: "
+  if train.type == :passenger
+    train.each_wagon { |wagon, i| puts "№ #{i+1} - #{Wagon::TYPE[wagon.type]} - Свободно: #{wagon.free_places} мест - Занято: #{wagon.busy_places} мест" }
+  else
+    train.each_wagon { |wagon, i| puts "№ #{i+1} - #{Wagon::TYPE[wagon.type]} - Свободно: #{wagon.free_capacity} тонн - Занято: #{wagon.busy_capacity} тонн" }
+  end
+end
+
+# Выводим поезда и вагоны на станциях
+st1.each_train (train_block)
+st2.each_train (train_block)
+
