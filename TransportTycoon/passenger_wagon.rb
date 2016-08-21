3a536081d5cc30231bfd77dd527a0664af923532
Botanik1592 <anton.buryka@gmail.com>
2016-08-21 14:51:41 +0300

    Добавлены атрибуты вместимости и занятых мест. Добавлен метод занимающий место, и методы, возвращающие кол-во свободных и занятых мест

--- TransportTycoon/passenger_wagon.rb
+++ TransportTycoon/passenger_wagon.rb
@@ -1,11 +1,22 @@
 class PassengerWagon < Wagon
 
+  attr_reader :capacity, :busy_places
+
+  def initialize (capacity=48)
+    @capacity = capacity
+    @busy_places = 0
+  end
+
   def type
     :passenger
   end
 
-  def wagon_capacity
-    48
+  def free_places
+    @capacity - @busy_places
+  end
+
+  def take_place
+    @busy_places += 1 if @busy_places < @capacity
   end
 
 end
