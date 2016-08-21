c63ee7e9a8f35c93c1a87d5508f43f4a1fbe7ca3
Botanik1592 <anton.buryka@gmail.com>
2016-08-21 14:52:48 +0300

    Добавлены атрибуты загрузки и занятого места. Добавлены методы, загружающие вагон, возвращающие свободное и занятое место

--- TransportTycoon/cargo_wagon.rb
+++ TransportTycoon/cargo_wagon.rb
@@ -1,11 +1,22 @@
 class CargoWagon < Wagon
 
+  attr_reader :load_capacity, :busy_capacity
+
+  def initialize (load_capacity = 56)
+    @load_capacity = load_capacity
+    @busy_capacity = 0
+  end
+
   def type
     :cargo
   end
 
-  def wagon_load_capacity
-    56
+  def free_capacity
+    @load_capacity - @busy_capacity
+  end
+
+  def load(num)
+    @busy_capacity += num if @busy_capacity + num <= @load_capacity
   end
 
 end
