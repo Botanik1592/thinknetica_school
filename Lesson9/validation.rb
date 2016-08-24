c80a8ba36ba2b8468532b047f6b0960e7fe1ad2f
Botanik1592 <anton.buryka@gmail.com>
2016-08-24 23:22:02 +0300

    Добавлены методы валидации.

--- Lesson9/validation.rb
+++ Lesson9/validation.rb
@@ -6,7 +6,29 @@ module Validation
 
   module ClassMethods
     def validate(name, *args)
-      var_name = "@validates"
-      instance_variable_set(var_name)
+      instance_variable_get(@validates)
+      instance_variable_set(@validates)[name] = *args
     end
+  end
+
+  module InstanceMethods
+    def validate!
+      @validates.each do |name, args|
+        value = instance_variable_get("@#{name}")
+        if args[0] == "presence"
+          raise "Argument is empty!" if value.nil? || value.empty?
+        elif args[0] == "format"
+          raise "Invalid format" if value =~ args[1]
+        else
+          raise "Invalid type" if self.class != args[1]
+        end
+      end
+    end
+
+    def valid?
+      validate!
+    rescue ArgumentError
+      fail
+    end
+  end
 end
