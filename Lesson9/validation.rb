19e1ac87babe5a0a242ff4d6c260d12c9d7d6c7a
Botanik1592 <anton.buryka@gmail.com>
2016-08-25 13:11:09 +0300

    Модуль валидации

--- Lesson9/validation.rb
+++ Lesson9/validation.rb
@@ -5,30 +5,42 @@ module Validation
   end
 
   module ClassMethods
+
     def validate(name, *args)
-      instance_variable_get(@validates)
-      instance_variable_set(@validates)[name] = *args
+      validates = '@@validates'
+      class_variable_set(validates, {})
+      class_variable_get(validates)[name] = *args
     end
   end
 
   module InstanceMethods
+
     def validate!
-      @validates.each do |name, args|
-        value = instance_variable_get("@#{name}")
-        if args[0] == "presence"
-          raise "Argument is empty!" if value.nil? || value.empty?
-        elif args[0] == "format"
-          raise "Invalid format" if value =~ args[1]
-        else
-          raise "Invalid type" if self.class != args[1]
-        end
+      self.class.class_variable_get('@@validates').each do |name, args|
+        send("validate_#{args[0]}", name, *args[1, args.size])
       end
+      true
+    end
+
+    def validate_presence(name)
+      value = instance_variable_get("@#{name}")
+      raise 'Argument is empty' if value.nil? || value.empty?
+    end
+
+    def validate_format(name, format)
+      value = instance_variable_get("@#{name}")
+      raise "Invalid format" if value !~ format
+    end
+
+    def validate_type(name, type)
+      type_class = instance_variable_get("@#{name}").to_s
+      raise "Invalid Type" if type_class != type
     end
 
     def valid?
       validate!
     rescue ArgumentError
-      fail
+      false
     end
   end
 end
