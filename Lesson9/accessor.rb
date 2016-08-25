45ef05a4c187a32aebbea481f0dcb7ac5ea39f5c
Botanik1592 <anton.buryka@gmail.com>
2016-08-25 22:20:07 +0300

    Исправлен метод strong_attr_accessor

--- Lesson9/accessor.rb
+++ Lesson9/accessor.rb
@@ -1,41 +1,37 @@
 module Accessors
   def self.included(base)
     base.extend ClassMethods
-    base.send :include, InstanceMethods
-  end
-
-  def initialize
-    @history = {}
+    #base.send :include, InstanceMethods
   end
 
   module ClassMethods
     def attr_accessor_with_history(*args)
       args.each do |name|
         var_name = "@#{name}".to_sym
-        var_name_set = "#{name}=".to_sym
+        var_history = "@#{name}_history".to_sym
+
         define_method(name) { instance_variable_get(var_name) }
 
-        define_method(var_name_set) do |value|
+        define_method("#{name}=") do |value|
+          instance_variable_get(var_history).nil? ? instance_variable_set(var_history, []) : instance_variable_get(var_history) << instance_variable_get(var_name)
           instance_variable_set(var_name, value)
-          @history[name] ||= []
-          @history[name] << value
-          add_history_method(name)
         end
+
+        define_method("#{name}_history") { instance_variable_get var_history }
       end
     end
 
     def strong_attr_accessor(name, type)
       var_name = "@#{name}".to_sym
       define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=".to_sym) do |value|
-        "#{value.class}".to_sym == type ? instance_variable_set(var_name, value) : raise("ClassesMismatch")
-        end
-    end
-  end
 
-  module InstanceMethods
-    def add_history_method(name)
-      define_singleton_method("#{name}_history") { @history[name] }
+      define_method("#{name}=") do |value|
+        p value.class
+        value.class == type ? instance_variable_set(var_name, value) : raise("ClassesMismatch")
+
+      end
     end
   end
 end
+
+
