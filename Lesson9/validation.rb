91345ceee4ffda83748bb79e0fe760b989ae0293
Botanik1592 <anton.buryka@gmail.com>
2016-08-25 22:19:17 +0300

    Исправлено замечание: Не надо получать значение по имени в методах валидации. Сделай это в методе validate!, а в эти методы просто передай полученное значение

--- Lesson9/validation.rb
+++ Lesson9/validation.rb
@@ -17,23 +17,24 @@ module Validation
 
     def validate!
       self.class.class_variable_get('@@validates').each do |name, args|
-        send("validate_#{args[0]}", name, *args[1, args.size])
+        value = instance_variable_get("@#{name}").to_s
+        send("validate_#{args[0]}", value, *args[1, args.size])
       end
       true
     end
 
-    def validate_presence(name)
-      value = instance_variable_get("@#{name}")
+    def validate_presence(val)
+      value = val
       raise 'Argument is empty' if value.nil? || value.empty?
     end
 
-    def validate_format(name, format)
-      value = instance_variable_get("@#{name}")
+    def validate_format(val, format)
+      value = val
       raise "Invalid format" if value !~ format
     end
 
-    def validate_type(name, type)
-      type_class = instance_variable_get("@#{name}").to_s
+    def validate_type(val, type)
+      type_class = val
       raise "Invalid Type" if type_class != type
     end
 
